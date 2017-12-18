6909d0ad4dc0a3561732f4d27c2d5b3b8437950a
Qudsia Ahmad <q4hmad@gmail.com>
2017-12-17 20:27:27 -0800

    add Role, username column to User model, and populate database with products, one admin and one client user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
-Product.destroy_all
+Role.create(name: :admin)
+Role.create(name: :client)
 
-15.times do
-  Product.create!(name: Faker::Coffee.variety)
+user1 = User.create(username: 'Nicole',
+								    email: 'admin@gmail.com',
+								    password: 'password1234',
+								    password_confirmation: 'password1234')
+user1.add_role(:admin)
+user2 = User.create(username: 'Bruce',
+								    email: 'client@gmail.com',
+								    password: 'password1234',
+								    password_confirmation: 'password1234')
+user2.add_role(:client)
+1.upto(5) do |i|
+	Product.create(name: "monitor #{i}",
+								 price: 600 * i,
+								 user: user1)
 end
-   p "Created #{Product.count} products"
+1.upto(5) do |i|
+	Product.create(name: "keyboard #{i}",
+								 price: 200 * i,
+								 user: user2)
+end
